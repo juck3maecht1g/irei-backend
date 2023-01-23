fc2681284561a68365916862fbec7b428c6a439e
Simonhil <sihi@outlook.de>
2023-01-23 18:00:00 +0100

    runnsRreact

--- src/controller/IREI.py
+++ src/controller/IREI.py
@@ -1,8 +1,10 @@
+
 from __init__ import app
 
 
 if __name__ == "__main__":
-   
+  
+
     app.run(debug=True, port= 5000)
 
         
--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -1,9 +1,16 @@
-from flask import Flask
+import os
+import webbrowser
+from flask import Flask, render_template
 from flask_cors import CORS
 
 
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder="../../build", static_url_path="/")
 CORS(app)
 
-from routes import *
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
+import ControlPageController
+
