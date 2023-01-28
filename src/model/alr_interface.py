c63f0833c2bb117145459b58cb4635bd57e2f8a9
Simonhil <sihi@outlook.de>
2023-01-28 14:57:27 +0100

    simon

--- src/model/alr_interface.py
+++ src/model/alr_interface.py
@@ -1,4 +1,4 @@
-from model.position import Position
+from src.model.communication.position.position import Position
 
 class alr_interface:
     #cunstruktor
@@ -6,23 +6,30 @@ class alr_interface:
 
     def __init__(self, active_experiment):
         self.active_experiment = active_experiment
+        #self.active_experiment = active_experiment
+        pass
+        
 
     def start_log(self):
-        self.active_experiment.start_log()
-
+        #self.active_experiment.start_log()
+        print("start")
 
     def cancel_log(self):
-        self.active_experiment.cancel_log()
-        
+        #self.active_experiment.cancel_log()
+        print("logging canceled")
         
     def stop_log(self):
          #self.active_experiment.stop_log()
+         print("logger stopped dict with data recived")
          return {"log1": "234715623478568127"} # to be deleted
     
     def reset_scene(self):
-        self.active_experiment.reset()
-
+        #self.active_experiment.reset()
+        print("scene reset")
    
+
+
+    # maby delete those methods
     def wait(self, ip, time):
         self.active_experiment.wait(ip, time)
 
@@ -38,8 +45,8 @@ class alr_interface:
         else: self.active_experiment.approach_joint(ip, position.get_joint)
 
     def save_posiiton(self, ip):
-        cartesian = self.get_cartesian_position_of(ip)
-        joint = self.get_joint_position_of(ip)
-        position = Position(cartesian, joint)
-
+        #cartesian = self.get_cartesian_position_of(ip)
+        #joint = self.get_joint_position_of(ip)
+        #position = Position(cartesian, joint)
+        print("saved position in alr-sim")
         return {"wanttobePosition": "asdghlfgjsdkjfh"}
