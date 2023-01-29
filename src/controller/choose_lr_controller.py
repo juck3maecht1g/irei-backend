4df3a93fa2b4c5263b0fd7fb6d7c7f3b27c77266
= <94857495+ucfjb@users.noreply.github.com>
2023-01-29 15:30:56 +0100

    implemented funcitons in lr controller

--- src/controller/choose_lr_controller.py
+++ src/controller/choose_lr_controller.py
@@ -1,6 +1,9 @@
 from src.controller.__init__ import app
 from flask import request
 from src.model.communication.physical.laboratory import Lab
+from src.controller.irei import get_registered_experiments, setup_experiment
+from src.model.communication.physical.robot import Robot
+from src.model.communication.physical.laboratory import Lab
 
 
 class ChooseLRController:
@@ -21,16 +24,14 @@ class ChooseLRController:
 
     @app.route("/api/" + lab_robots_marker)
     @staticmethod
-    def get_robots_list():  # rem param
+    def get_robots_list():
         if ChooseLRController.current_lab == None:
             return []
         to_return = []
         robots = ChooseLRController.current_lab.get_robots()
         for robot in robots:
-            to_append = {"name" : robot.get_name, "ip": robot.get_ip}
+            to_append = {"name": robot.get_name, "ip": robot.get_ip}
             to_return.append(to_append)
-        # for robot in robots:
-        #    to_return.append(robot.get_ip)
 
         return to_return
 
@@ -58,6 +59,101 @@ class ChooseLRController:
         for lab in test_labs:
             if lab.get_name() == data:
                 ChooseLRController.current_lab = lab
+            return 'Done', 201
+
+        return 'failed', 201
+
+    robots_exp_marker = "getRobotsInExperiment"
+
+    @app.route("/api/" + robots_exp_marker)
+    @staticmethod
+    def get_robots_exp():
+        data = ChooseLRController.experiment_config_handler.get_exp_robots()
+        to_return = dict()
+        for robot in data:
+            to_append = {"name": robot.get_name, "ip": robot.get_ip}
+            to_return.append(to_append)
+        return to_return
+
+    robots_gripper_marker = "getRobotsForGripper"
+
+    @app.route("/api/" + robots_gripper_marker)
+    @staticmethod
+    def get_robots_gripper():
+        data = ChooseLRController.experiment_config_handler.get_execute_gripper()
+        to_return = dict()
+        for robot in data:
+            to_append = {"name": robot.get_name, "ip": robot.get_ip}
+            to_return.append(to_append)
+        return to_return
+
+    reg_exp_marker = "getRegExp"
+
+    @app.route("/api/" + reg_exp_marker)
+    @staticmethod
+    def get_reg_exp():
+        data = get_registered_experiments()
+        to_return = []
+        for exp in data:
+            to_return.append(exp.get_name())
+        return to_return
+
+    setup_exp_marker = "setup_exp"
+
+    @app.route("/api/" + setup_exp_marker, methods=['POST'])
+    @staticmethod
+    def setup_exp():
+        data = request.get_json()
+        if data in get_registered_experiments:
+            setup_experiment(data)
+            return 'Done', 201
+        else:
+            return 'failed', 201
+
+    set_robots_exp_marker = "setRobotsExp"
+
+    @app.route("/api/" + set_robots_exp_marker, methods=['POST'])
+    @staticmethod
+    def set_robots_exp():
+        data = request.get_json()
+        robots = []
+        for ip in data:
+            for robot in ChooseLRController.current_lab.get_robots():
+                if ip == robot.get_ip():
+                    robots.append(Robot(ip=ip, name=robot.get_name()))
+        ChooseLRController.experiment_config_handler.set_exp_robots(robots)
+        return 'Done', 201
+
+        # todo error condition
+
+    set_robots_gripper_marker = "setRobotsGripper"
+
+    @app.route("/api/" + set_robots_gripper_marker, methods=['POST'])
+    @staticmethod
+    def set_robots_gripper():
+        data = request.get_json()
+        robots = []
+        for ip in data:
+            for robot in ChooseLRController.experiment_config_handler.get_exp_robots():
+                if ip == robot.get_ip():
+                    robots.append(Robot(ip=ip, name=robot.get_name()))
+        ChooseLRController.experiment_config_handler.set_execute_gripper(
+            robots)
+        return 'Done', 201
+
+        # todo error condition
+
+    set_save_pos_marker = "setRobotsGripper"
+
+    @app.route("/api/" + set_save_pos_marker, methods=['POST'])
+    @staticmethod
+    def set_save_pos():
+        data = request.get_json()
+        for robot in ChooseLRController.experiment_config_handler.get_exp_robots():
+            if data == robot.get_ip():
+                pos_robot = Robot(ip=data, name=robot.get_name())
+                ChooseLRController.experiment_config_handler.set_save_position_robot(
+                    pos_robot)
                 return 'Done', 201
 
         return 'failed', 201
