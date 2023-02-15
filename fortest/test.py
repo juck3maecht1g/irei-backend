650adb1c6af5ba70c68df410b95b7102d27a9568
Simonhil <sihi@outlook.de>
2023-02-15 22:28:45 +0100

    test

--- fortest/test.py
+++ fortest/test.py
@@ -1,3 +1,4 @@
+from src.controller.fetch_for_action import FetchForAction
 from src.model.file_storage.pc_data_handler import PcDataHandler
 from src.model.file_storage.experiment_config_handler import ExperimentConfigHandler
 from src.model.file_storage.global_config_handler import GlobalConfigHandler
@@ -9,10 +10,19 @@ pc_data = PcDataHandler(root_path)
 pc_data.attach(exp_config)
 
 
-if not ("global_config" in pc_data.get_dir_content()):
-        global_config.create()
-        for user in global_config.get_users():
-            pc_data.create_directory(user)
-            pc_data.navigate_to_child(user)
-            exp_config.create()
-            pc_data.navigate_to_parent()
+# if not ("global_config" in pc_data.get_dir_content()):
+#         global_config.create()
+#         for user in global_config.get_users():
+#             pc_data.create_directory(user)
+#             pc_data.navigate_to_child(user)
+#             exp_config.create()
+#             pc_data.navigate_to_parent()
+
+
+
+map = {0: "ip0", "sublist": [{1: "ip1"}]}
+
+#print(FetchForAction.replace_sub_list_buttom_up(map, {1: "ip7"}, [0]) )
+print(FetchForAction.set_mapping_in_table())
+data = [{"0":234780}, {"1": 47823}]
+total= {0: "ip0", 1: "ip0","sublist": [{}, {0: "ip0", 1: "ip0", "sublist": [{0: "ip0"}]}]}
